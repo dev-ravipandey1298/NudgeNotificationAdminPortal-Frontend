c522f1df53bf20df7faa77de68d9ebadf5368e7b
Ravi Pandey <dev.ravipandey1298@gmail.com>
2024-09-13 12:29:19 +0000

    Login

--- NudgeNotificationAdminPortal/src/services/templateService.js
+++ NudgeNotificationAdminPortal/src/services/templateService.js
@@ -10,97 +10,150 @@ const apiClient = axios.create({
   },
 });
 
-apiClient.interceptors.request.use(
-  (config) => {
+// apiClient.interceptors.request.use(
+//   (config) => {
     
-    const token = sessionStorage.getItem('authToken'); 
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
+//     const token = sessionStorage.getItem('authToken'); 
+//     if (token) {
+//       config.headers.Authorization = `Bearer ${token}`;
+//     }
+//     return config;
+//   },
+//   (error) => {
+//     return Promise.reject(error);
+//   }
+// );
 
 
-apiClient.interceptors.response.use(
-  (response) => {
-    return response.data;
-  },
-  (error) => {
-    // Handle errors globally
-    if (error.response && error.response.status === 401) {
-      // Handle unauthorized errors
-      console.error('Unauthorized access - logging out...');
-      // Redirect to login page or show a notification
-    }
-    return Promise.reject(error);
-  }
-);
+// apiClient.interceptors.response.use(
+//   (response) => {
+//     return response.data;
+//   },
+//   (error) => {
+//     // Handle errors globally
+//     if (error.response && error.response.status === 401) {
+//       // Handle unauthorized errors
+//       console.error('Unauthorized access - logging out...');
+//       // Redirect to login page or show a notification
+//     }
+//     return Promise.reject(error);
+//   }
+// );
+
+ 
 
 // Define API request functions
 export const fetchTemplates = () => {
-  return apiClient.get('/v1/templates'); // GET request to /templates
+  return apiClient.get('/v1/templates', {
+    headers: {
+      'X-Auth-Notification-Portal' : `Bearer ${sessionStorage.getItem('authToken')}`,
+    },
+  }); 
 };
 
 export const getTemplateById = (templateId) => {
-  return apiClient.get(`/v1/templates/draft/${templateId}`); // GET request to /templates/:templateId
+  return apiClient.get(`/v1/templates/draft/${templateId}`, {
+    headers: {
+      'X-Auth-Notification-Portal' : `Bearer ${sessionStorage.getItem('authToken')}`,
+    },
+  }); 
 };
 
 export const getAllTemplates = () => {
-  return apiClient.get(`/v1/templates/draft`); // GET All
+  return apiClient.get(`/v1/templates/draft`, {
+    headers: {
+      'X-Auth-Notification-Portal' : `Bearer ${sessionStorage.getItem('authToken')}`,
+    },
+  }); 
 };
-
 export const createTemplate = (templateData) => {
-  return apiClient.post('/v1/templates/draft', templateData); // POST request to /templates
+  return apiClient.post('/v1/templates/draft', templateData, {
+    headers: {
+      'X-Auth-Notification-Portal' : `Bearer ${sessionStorage.getItem('authToken')}`,
+    },
+  }); 
 };
 
 export const updateTemplate = (templateId, templateData) => {
-  return apiClient.put(`/v1/templates/draft/${templateId}`, templateData); // PUT request to /templates/:templateId
+  return apiClient.put(`/v1/templates/draft/${templateId}`, templateData, {
+    headers: {
+      'X-Auth-Notification-Portal' : `Bearer ${sessionStorage.getItem('authToken')}`,
+    },
+  }); 
 };
 
 export const deleteTemplate = (templateId) => {
-  return apiClient.delete(`/v1/templates/draft/${templateId}`); // DELETE request to /templates/:templateId
+  return apiClient.delete(`/v1/templates/draft/${templateId}`, {
+    headers: {
+      'X-Auth-Notification-Portal' : `Bearer ${sessionStorage.getItem('authToken')}`,
+    },
+  }); 
 };
 
 export const submitForCUG_Approval_Template = (templateData) => {
-  return apiClient.put(`/v1/templates/cug-approval`, templateData); // PUT request to /templates/cug-approval
+  return apiClient.put(`/v1/templates/cug-approval`, templateData, {
+    headers: {
+      'X-Auth-Notification-Portal' : `Bearer ${sessionStorage.getItem('authToken')}`,
+    },
+  }); 
 };
 
 export const getAllSearchTemplate = () => {
-  return apiClient.get(`/v1/templates?status=APPROVAL_PENDING_CUG&status=APPROVAL_PENDING_PROD`); // GET request to /templates?status=
+  return apiClient.get(`/v1/templates?status=APPROVAL_PENDING_CUG&status=APPROVAL_PENDING_PROD`, {
+    headers: {
+      'X-Auth-Notification-Portal' : `Bearer ${sessionStorage.getItem('authToken')}`,
+    },
+  }); 
 };
 
 export const getAllSearchActionTemplate = () => {
-  return apiClient.get(`/v1/templates?status=CUG_APPROVED&status=REJECTED&status=CUG_FAILED`); // GET request to /templates?status=
+  return apiClient.get(`/v1/templates?status=CUG_APPROVED&status=REJECTED&status=CUG_FAILED`, {
+    headers: {
+      'X-Auth-Notification-Portal' : `Bearer ${sessionStorage.getItem('authToken')}`,
+    },
+  }); 
 };
 
 export const markCUGReject = (templateId, comment) => {
-  return apiClient.patch(`/v1/templates/${templateId}/cug-reject`, comment); // PATCH request to /templates?status=
+  return apiClient.patch(`/v1/templates/${templateId}/cug-reject`, comment, {
+    headers: {
+      'X-Auth-Notification-Portal' : `Bearer ${sessionStorage.getItem('authToken')}`,
+    },
+  }); 
 };
 
 export const markCUGApproved = (templateId, comment) => {
-  return apiClient.patch(`/v1/templates/${templateId}/cug-approved`, comment); // PATCH request to /templates?status=
+  return apiClient.patch(`/v1/templates/${templateId}/cug-approved`, comment, {
+    headers: {
+      'X-Auth-Notification-Portal' : `Bearer ${sessionStorage.getItem('authToken')}`,
+    },
+  }); 
 };
-
 
 export const submitForPRODApproval = (templateId, formData) => {
   return apiClient.patch(`/v1/templates/${templateId}/final-approval`, formData, {
     headers: {
       'Content-Type': 'multipart/form-data',
+      'X-Auth-Notification-Portal' : `Bearer ${sessionStorage.getItem('authToken')}`,
     },
   }); 
 
 };
 
 export const markPRODReject = (templateId, comment) => {
-  return apiClient.patch(`/v1/templates/${templateId}/final-reject`, comment); // PATCH request to /templates?status=
+  return apiClient.patch(`/v1/templates/${templateId}/final-reject`, comment, {
+    headers: {
+      'X-Auth-Notification-Portal' : `Bearer ${sessionStorage.getItem('authToken')}`,
+    },
+  }); 
 };
 
 export const markPRODApproved = (templateId, comment) => {
-  return apiClient.patch(`/v1/templates/${templateId}/final-approved`, comment); // PATCH request to /templates?status=
+  return apiClient.patch(`/v1/templates/${templateId}/final-approved`, comment, {
+    headers: {
+      'X-Auth-Notification-Portal' : `Bearer ${sessionStorage.getItem('authToken')}`,
+    },
+  }); 
 };
 
 // LOGIN API's
