bc1d6e122875eb810f23f34d755c3292050fca2d
Ravi Pandey <dev.ravipandey1298@gmail.com>
2024-09-20 15:12:57 +0000

    LOGIN

--- NudgeNotificationAdminPortal/src/constants/statusConstant.js
+++ NudgeNotificationAdminPortal/src/constants/statusConstant.js
@@ -1,9 +1,9 @@
 export const status = {
     DRAFT: "Draft",
     PROD_APPROVED : "Prod Approved",
-    APPROVAL_PENDING_CUG : "Submitted Cug Approval",
-    CUG_APPROVED : "Cug Approved",
+    APPROVAL_PENDING_CUG : "Submitted for CUG Approval",
+    CUG_APPROVED : "CUG Approved",
     REJECTED : "Rejected",
-    CUG_FAILED : "Cug failed",
-    APPROVAL_PENDING_PROD : "Submitted Prod Approval"
+    CUG_FAILED : "CUG failed",
+    APPROVAL_PENDING_PROD : "Submitted for Prod Approval"
   }
